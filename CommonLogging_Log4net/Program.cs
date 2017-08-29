b429ce525418f2e67a0681ee13673e81d5357920
JerryShih <jerryshih@tutorabc.com>
2017-08-29 11:59:23 +0800

    log4net 設定

--- CommonLogging_Log4net/Program.cs
+++ CommonLogging_Log4net/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,11 @@ namespace CommonLogging_Log4net
 {
     class Program
     {
+        private static ILog logger = LogManager.GetLogger(typeof(Program));
+
         static void Main(string[] args)
         {
+            logger.Info("沒有使用Common.Logging  Log4net Hello");
         }
     }
 }
