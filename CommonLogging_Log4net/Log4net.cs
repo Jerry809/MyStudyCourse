894ccd8db402019a27196757ccd05fa19f0e9462
JerryShih <jerryshih@tutorabc.com>
2017-08-29 12:25:58 +0800

    CommonLogging log4net 練習, 及log4net練習

---
+++ CommonLogging_Log4net/Log4net.cs
@@ -0,0 +1,19 @@
+﻿using Common.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLogging_Log4net
+{
+    public class Log4net
+    {
+        private ILog log = LogManager.GetLogger<Log4net>();
+
+        public void Info(string message)
+        {
+            log.Info(message);
+        }
+    }
+}
--- CommonLogging_Log4net/Program.cs
+++ CommonLogging_Log4net/Program.cs
@@ -1,6 +1,8 @@
 ﻿using log4net;
+using log4net.Config;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +11,29 @@ namespace CommonLogging_Log4net
 {
     class Program
     {
-        private static ILog logger = LogManager.GetLogger(typeof(Program));
+        //private static ILog logger = LogManager.GetLogger(typeof(Program));
 
         static void Main(string[] args)
         {
-            logger.Info("沒有使用Common.Logging  Log4net Hello");
+            // 取得Logger(Logger以Program的Type Name命名)
+            var log = LogManager.GetLogger(typeof(Program));
+
+            //BasicConfigurator.Configure();
+            // 因為要讀取從App.config讀取設定，所以使用XmlConfigurator
+            string log4netPath = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "../../log4net.config");
+            log4net.Config.XmlConfigurator.ConfigureAndWatch(new System.IO.FileInfo(log4netPath));
+            
+            for (var i = 0; i < 10; i++)
+            {
+                log.Info(i);
+                log.Debug(i);
+            }
+
+            var commonLogging = new Log4net();
+            commonLogging.Info("使用Common Logging Log4net Hello");
+
+
+            Console.ReadLine();
         }
     }
 }
