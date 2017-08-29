c46bf5d58bbdd2f5d96de26a320fa39da3357072
JerryShih <jerryshih@tutorabc.com>
2017-08-29 11:38:41 +0800

    CommonLogging 的NLog 練習 , 及一般NLog練習

---
+++ CommonLogging_NLog/CommonLoggingNLog.cs
@@ -0,0 +1,19 @@
+﻿using Common.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLogging_NLog
+{
+    public class CommonLoggingNLog
+    {
+        private ILog log = LogManager.GetLogger<CommonLoggingNLog>();
+
+        public void Trace(string message)
+        {
+            log.Trace(message);
+        }
+    }
+}
---
+++ CommonLogging_NLog/Program.cs
@@ -0,0 +1,23 @@
+﻿using Common.Logging;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLogging_NLog
+{
+    class Program
+    {
+        private static Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            logger.Trace("未使用Common.Logging NLog Trace");
+
+            var commonNLog = new CommonLoggingNLog();
+            commonNLog.Trace("使用Common.Logging NLog Trace");
+        }
+    }
+}
