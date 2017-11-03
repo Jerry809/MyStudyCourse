dff28640916bc9ed28ad99ddcda8e9202f02146e
jerry <jerry.shih@e-record.com.tw>
2017-11-03 10:44:46 +0800

    no message

--- CommonLogging_NLog/Program.cs
+++ CommonLogging_NLog/Program.cs
@@ -2,6 +2,7 @@
 using NLog;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,10 +15,40 @@ namespace CommonLogging_NLog
 
         static void Main(string[] args)
         {
-            logger.Trace("未使用Common.Logging NLog Trace");
+            //設定針測的檔案類型，如不指定可設定*.*
+            var watch = new FileSystemWatcher(@"C:\Sample","*.*");
 
-            var commonNLog = new CommonLoggingNLog();
-            commonNLog.Trace("使用Common.Logging NLog Trace");
+            //是否連子資料夾都要偵測
+            watch.IncludeSubdirectories = true;
+            watch.NotifyFilter = NotifyFilters.FileName | NotifyFilters.DirectoryName ;
+
+            watch.InternalBufferSize = 2 * 1000 * 1024;
+            //開啟監聽
+            watch.EnableRaisingEvents = true;
+
+            Console.WriteLine($"File  Watching..... Start  [{DateTime.Now}");
+        
+            //新增時觸發事件
+            watch.Created += (
+                (object sender, FileSystemEventArgs e) =>
+                {
+                    Console.WriteLine("Created...");
+                    var fileName = e.FullPath;
+                    if (File.Exists(fileName))
+                    {
+                        logger.Trace(fileName);
+                        File.Move(fileName, fileName + "temp");
+                        Console.WriteLine($"FullPath = {e.FullPath} , Name =  {e.Name}");
+                    }                   
+                });
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+            Console.WriteLine($"File  Watching..... End  [{DateTime.Now}");
+
+            //logger.Trace("未使用Common.Logging NLog Trace");
+
+            //var commonNLog = new CommonLoggingNLog();
+            //commonNLog.Trace("使用Common.Logging NLog Trace");
         }
     }
 }
--- Log4Slack_Course/Program.cs
+++ Log4Slack_Course/Program.cs
@@ -1,5 +1,4 @@
 ﻿using log4net;
-using log4net.Config;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -7,12 +6,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CommonLogging_Log4net
+namespace Log4Slack_Course
 {
     class Program
     {
-        //private static ILog logger = LogManager.GetLogger(typeof(Program));
-
+        private string webHookUrl = "https://hooks.slack.com/services/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";
         static void Main(string[] args)
         {
             // 取得Logger(Logger以Program的Type Name命名)
@@ -22,18 +20,13 @@ namespace CommonLogging_Log4net
             // 因為要讀取從App.config讀取設定，所以使用XmlConfigurator
             string log4netPath = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "../../log4net.config");
             log4net.Config.XmlConfigurator.ConfigureAndWatch(new System.IO.FileInfo(log4netPath));
-            
+
             for (var i = 0; i < 10; i++)
             {
                 log.Info(i);
                 log.Debug(i);
             }
 
-            //var commonLogging = new Log4net();
-            //commonLogging.Info("使用Common Logging Log4net Hello");
-
-
-            Console.ReadLine();
         }
     }
 }
